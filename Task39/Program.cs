6f54b9246f3c5f48ff4cfe55ab9d5c24a9034630
Мега Крутой <krutoi.chuvak1918@gmail.com>
2023-10-08 19:02:24 +0300

    V1

---
+++ Task39/Program.cs
@@ -0,0 +1,43 @@
+﻿// // Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] PullArray(int[] array) { // Функция заполняет массив рандомные числами от 1 до 10
+    var rnd = new Random();
+    int length = array.Length;
+    for(int i = 0; i < length; i++) {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+
+void Reverse(int[] array) { // Функция делает реверс массива
+    int length = array.Length;
+    for(int i = 0; i < length / 2; i++) {
+        (array[length - 1 - i], array[i]) = (array[i], array[length - 1 - i]);
+    }
+}
+
+void PrintArray(int[] array) { // Функция выводит массив
+    Console.Write("[");
+    int length = array.Length;
+    for(int i = 0; i < length; i++) {
+        Console.Write(array[i]);
+        if(i != length - 1) {
+            Console.Write(" ");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] array = new int[6];
+PullArray(array);
+PrintArray(array);
+Console.WriteLine();
+Reverse(array);
+PrintArray(array);
+Console.WriteLine();
+Array.Reverse(array);
+PrintArray(array);
---
+++ Task40/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник со сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+bool CheckTriangle(int a, int b, int c) {
+    return a + b > c && b + c > a && a + c > b;
+}
+
+Console.Write("Введите значение первой стороны треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение второй стороны треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение третьей стороны треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+bool check = CheckTriangle(a, b, c);
+if(check == true) {
+    Console.WriteLine("Треугольник со значениями данных сторон возможен");
+} else {
+    Console.WriteLine("Треугольник со значениями данных сторон невозможен");
+}
